c9a5297d8e0daa94ba31288146546e83532ce1af
Ilias Van Peer <ilias@idlemonkeys.net>
2012-05-15 21:12:05 +0200

    Support showing usage on failure

--- src/main/java/khl/dip/assignment/App.java
+++ src/main/java/khl/dip/assignment/App.java
@@ -2,8 +2,6 @@ package khl.dip.assignment;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
-import ij.ImagePlus;
-import ij.gui.ImageWindow;
 
 /**
  * Hello world!
@@ -12,19 +10,21 @@ import ij.gui.ImageWindow;
 public class App 
 {
     public static void main(String[] args) { 
+        Carve carve = new Carve();
+        JCommander jc = new JCommander();
+        jc.addObject(carve);
+        StringBuilder usage = new StringBuilder();
+        jc.usage(usage);
+        
         try {
-            Carve carve = new Carve();
-            JCommander jc = new JCommander(carve, args);
+            jc.parse(args);
             carve.run();
             //Carve carve = new Carve(img);
             //carve.benchmark(5000);
             //carve.benchmarkImportance(500);
-            ImagePlus img = carve.getImage();
-
-            ImageWindow window = new ImageWindow(img);
-            window.setVisible(true);
         } catch (ParameterException ex) {
-            System.out.println(ex.getMessage());
+            usage.insert(0, ex.getMessage() + "\n\n");
+            System.out.println(usage);
         }
     }
 }
