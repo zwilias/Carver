2be0efc9fbca6c5a12197bb78a85a20068c318ef
Ilias Van Peer <ilias@idlemonkeys.net>
2012-05-20 15:58:18 +0200

    clean imports

--- src/main/java/khl/dip/assignment/ShapeCreator.java
+++ src/main/java/khl/dip/assignment/ShapeCreator.java
@@ -2,7 +2,6 @@
 package khl.dip.assignment;
 
 import com.beust.jcommander.ParameterException;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
